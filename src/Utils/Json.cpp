c86386112782b13cf40e5508d54efa3c2390299f
rackover <roxanne@thegamebakers.com>
2022-08-04 11:16:22 +0200

    Update materials json

--- src/Utils/Json.cpp
+++ src/Utils/Json.cpp
@@ -31,4 +31,34 @@ namespace Utils::Json
 			return "null";
 		}
 	}
+
+
+	int ReadFlags(const std::string binaryFlags)
+	{
+		int result = 0x00;
+		size_t size = sizeof(int) * 8;
+
+		if (binaryFlags.size() > size) {
+			Components::Logger::Print("Flag {} might not be properly translated, it seems to contain an error (invalid length)\n", binaryFlags);
+			return result;
+		}
+
+		size_t i = size - 1;
+		for (char bit : binaryFlags)
+		{
+			if (i < 0)
+			{
+				// Uhmm
+				Components::Logger::Print("Flag {} might not be properly translated, it seems to contain an error (invalid length)\n", binaryFlags);
+				break;
+			}
+
+			bool isOne = bit == '1';
+			result |= isOne << i;
+			i--;
+		}
+
+		return result;
+	}
+
 }
