273a4b0468ee0db6174d7240e7559a69303ca4d1
Louvenarde <louve@louve.systems>
2022-08-05 21:12:19 +0200

    Fix water

--- src/Utils/Json.hpp
+++ src/Utils/Json.hpp
@@ -3,5 +3,6 @@
 namespace Utils::Json
 {
 	std::string TypeToString(nlohmann::json::value_t type);
-	int ReadFlags(const std::string binaryFlags);
+
+	unsigned long ReadFlags(const std::string binaryFlags, const size_t size);
 }
