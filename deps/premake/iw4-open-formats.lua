34c83d9c4b852b6ba289e7c6a5d196bc67d93c5d
FutureRave <edoardo.sanguineti222@gmail.com>
2023-02-14 21:31:42 +0000

    fix premake5?

--- deps/premake/iw4-open-formats.lua
+++ deps/premake/iw4-open-formats.lua
@@ -1,67 +1,44 @@
-iw4of = {
-	source = path.join(dependencies.basePath, "iw4-open-formats", "src"),
-	dependenciesPath = path.join(dependencies.basePath, "iw4-open-formats", "deps")
+iw4_open_formats = {
+	source = path.join(dependencies.basePath, "iw4-open-formats/src"),
 }
 
-iw4of_dependencies = {
-	basePath = iw4of.dependenciesPath
-}
-	
+function iw4_open_formats.import()
+	links {"iw4-open-formats"}
 
-
-function iw4of.import()
-	links { "iw4of" }
-	
-	dir = path.join(dependencies.basePath, "iw4-open-formats", "deps", "premake/*.lua")
-	deps = os.matchfiles(dir)
-	
-	for i, dep in pairs(deps) do
-		dep = dep:gsub(".lua", "")
-		require(dep)
-	end
-	
-	iw4of.libtomcrypt.import()
-	iw4of.libtommath.import()
-	iw4of.rapidjson.import()
-	iw4of.zlib.import()
-	iw4of.zstd.import()
-	
-	
-	iw4of.includes()
+	iw4_open_formats.includes()
 end
 
-function iw4of.includes()
+function iw4_open_formats.includes()
 	includedirs {
-		path.join(dependencies.basePath, "iw4-open-formats", "include")
+		path.join(iw4_open_formats.source, "src/headers")
 	}
 end
 
-function iw4of.project()
-	project "iw4of"
-		warnings "Off"
-		kind "StaticLib"
+function iw4_open_formats.project()
+	project "iw4-open-formats"
 		language "C++"
-		cppdialect "C++latest"
 
-		pchheader "std_include.hpp" -- must be exactly same as used in #include directives
-		pchsource (path.join(iw4of.source, "iw4-of/std_include.cpp"))
-		
+		libtomcrypt.import()
+		ibtommath.import()
+		rapidjson.import()
+		zlib.import()
+		iw4_open_formats.includes()
+
+		pchheader "std_include.hpp"
+		pchsource path.join(iw4_open_formats.source, "iw4-of/std_include.cpp")
+
 		files {
-			path.join(iw4of.source, "iw4-of/**.hpp"),
-			path.join(iw4of.source, "iw4-of/**.cpp")
+			path.join(iw4_open_formats.source, "iw4-of/**.hpp"),
+			path.join(iw4_open_formats.source, "iw4-of/**.cpp").
 		}
-		
+
 		includedirs {
-			path.join(iw4of.source, "iw4-of"),
+			path.join(iw4_open_formats.source, "iw4-of"),
 			path.join(dependencies.basePath, "iw4-open-formats", "include")
 		}
-		
-		iw4of.libtomcrypt.includes()
-		iw4of.libtommath.includes()
-		iw4of.rapidjson.includes()
-		iw4of.zlib.includes()
-		iw4of.zstd.includes()
-		
+
+		warnings "Off"
+		kind "StaticLib"
 end
 
-table.insert(dependencies, iw4of)
+table.insert(dependencies, iw4_open_formats)
