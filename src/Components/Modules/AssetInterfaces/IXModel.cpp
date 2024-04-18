1edfeac2c21f578fa783d8cdfb1b2ede5dc95193
Roxanne <roxanne@thegamebakers.com>
2024-04-18 14:31:34 +0200

    Set shield hit location

--- src/Components/Modules/AssetInterfaces/IXModel.cpp
+++ src/Components/Modules/AssetInterfaces/IXModel.cpp
@@ -22,6 +22,19 @@ namespace Assets
 				Assets::IXModel::ConvertPlayerModelFromSingleplayerToMultiplayer(header->model, *builder->getAllocator());
 			}
 
+			// Fix hit location for shield
+			for (size_t i = 0; i < header->model->numBones; i++)
+			{
+				const auto name = header->model->boneNames[i];
+				const auto strName = Game::SL_ConvertToString(name);
+				// check for tags that should have the HITLOC_SHIELD hit location
+				// thanks to Killera
+				if (strName == "tag_weapon_left"s || strName == "tag_shield_back"s)
+				{
+					header->model->partClassification[i] = Game::HITLOC_SHIELD;
+				}
+			}
+
 			// ???
 			if (header->model->physCollmap)
 			{
